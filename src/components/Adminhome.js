3f43ff4c57bc9ac3097b86dbe5cc1d3e93e759ab
jesvinv4u <jesvinv4u@gmail.com>
2025-01-17 00:06:06 +0530

    commited

--- src/components/Adminhome.js
+++ src/components/Adminhome.js
@@ -29,20 +29,26 @@ function Adminhome() {
           <h3>Admin Panel</h3>
         </div>
         <ul className="nav-links">
-          <li className="item"><Link to="#">Dashboard</Link></li>
-          <li className="item"><Link to="#">User Management</Link></li>
-          <li className="item"><Link to="#">Reports</Link></li>
-          <li className="item"><Link to="#">Settings</Link></li>
+          <li className="item"><Link to="#">Complaint Management</Link></li>
+          <li className="item"><Link to="#">Outpass Management</Link></li>
+          <li className="item"><Link to="#">Manage Requests</Link></li>
           <li className="dropdown">
             <button onClick={toggleDropdown}>More</button>
-            {dropdown && (
-              <ul className="dropdown-menu">
-                <li><button onClick={handleLogout}>Logout</button></li>
-              </ul>
-            )}
-          </li>
-        </ul>
-      </div>
+                        {dropdown && (
+                          <ul className="dropdown-menu">
+                            <li><Link to="/">Logout</Link></li>
+                          </ul>
+                        )}
+                      </li>
+          </ul>
+                    <div className="profile">
+                      <div className="profile-icon">
+                        <img src="https://www.w3schools.com/w3images/avatar2.png" alt="Profile" />
+                      </div>
+                      <p>Admin</p>
+                    </div>
+                  </div>
+      
       <div className="main-content">
         <h2>Welcome to the Admin Dashboard</h2>
         {/* Add Admin-specific content here */}
